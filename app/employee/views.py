79f67f6d816f53315f846e2b49c87295fd56efdd
SH Emon <emon109848@gmail.com>
2024-09-15 05:06:38 +0600

    Resolve Image size

--- app/employee/views.py
+++ app/employee/views.py
@@ -18,6 +18,9 @@ from django_filters.rest_framework import (
 from core.models import Employee
 from employee import serializers
 
+from PIL import Image
+import io
+
 
 class EmployeeFilter(FilterSet):
     """Custom filter for Employee model to support partial filtering."""
@@ -49,7 +52,7 @@ class EmployeeViewSet(viewsets.ModelViewSet):
 
     @action(methods=['POST'], detail=True, url_path='upload-image')
     def upload_image(self, request, pk=None):
-        """Upload an image to Emplyee."""
+        """Upload an image to Employee."""
         employee = self.get_object()
         serializer = self.get_serializer(employee, data=request.data)
 
@@ -58,6 +61,25 @@ class EmployeeViewSet(viewsets.ModelViewSet):
                 {'error': 'No image provided'},
                 status=status.HTTP_400_BAD_REQUEST
             )
+
+        image = request.FILES['photo']
+
+        image_size_kb = image.size / 1024
+        max_size_kb = 200
+
+        if image_size_kb > max_size_kb:
+            image_file = Image.open(image)
+
+            if image_file.mode in ('RGBA', 'LA') and image_file.info.get('transparency') is not None:  # noqa
+                image_file = image_file.convert('RGB')
+
+            output_io = io.BytesIO()
+            image_file.save(output_io, format='JPEG', quality=85)
+
+            compressed_image = io.BytesIO(output_io.getvalue())
+            compressed_image.name = image.name  # Preserve original file name
+
+            request.FILES['photo'] = compressed_image
 
         if serializer.is_valid():
             serializer.save()
